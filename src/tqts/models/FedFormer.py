fix: Fixed scaling issue

src/tqts/models/FedFormer.py
@@ -6,10 +6,11 @@
 __author__ = "Dhanunjaya Elluri"
 __mail__ = "dhanunjaya.elluri@tu-dortmund.de"
 
-from typing import Tuple, Any
+from typing import Any
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from tqts.models.layers.autocorrelation import AutoCorrelationLayer
 from tqts.models.layers.auxiliary import SeriesDeCompMulti, SeriesDeComp, MyLayerNorm

src/tqts/models/Transformer.py
@@ -6,7 +6,7 @@
 __author__ = "Dhanunjaya Elluri"
 __mail__ = "dhanunjaya.elluri@tu-dortmund.de"
 
-from typing import Tuple, Any
+from typing import Any
 
 import torch
 import torch.nn as nn